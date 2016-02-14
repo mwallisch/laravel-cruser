de9123cc81ed50a7ccc6af232379dca7105a06ff
ives_markus <markus.wallisch@interactives.eu>
2016-02-14 16:12:30 +0100

    Remove Composer, use simplified src directory

--- src/CreateUser.php
+++ src/CreateUser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ives\Laravel\Console;
+namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Autoload dependencies
-require 'vendor/autoload.php';
-
-$command = new \Ives\Laravel\Console\CreateUser();
